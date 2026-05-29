cb4ae0ef725af859e4366477e9e1d97f9fbb80c7
Muhammad-Zohaib-Malik <muhammadzohaibmalik10@gmail.com>
2026-05-30 00:00:37 +0500

    fix: update CORS configuration to allow multiple origins

--- inventory-service/src/app.js
+++ inventory-service/src/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://meredukaan.netlify.app/","http://localhost:5173"],
     credentials: true,
   }),
 );
