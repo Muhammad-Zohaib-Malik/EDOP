be589adb841c604758169672f7cf9436b231bc02
Muhammad-Zohaib-Malik <muhammadzohaibmalik10@gmail.com>
2026-05-29 22:56:00 +0500

    refactor: simplify database connection configuration by using DATABASE_URL

--- inventory-service/src/config/db.js
+++ inventory-service/src/config/db.js
@@ -1,11 +1,15 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
+  // host: process.env.PG_HOST,
+  // user: process.env.PG_USER,
+  // password: process.env.PG_PASSWORD,
+  // database: process.env.PG_DATABASE,
+  // port: process.env.PG_PORT,
+  connectionString:process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const connectDatabase = async () => {
