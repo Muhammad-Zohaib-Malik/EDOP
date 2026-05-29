e8bf7ae8dd8481f573743464c590fc4e9c4b41d9
Muhammad-Zohaib-Malik <muhammadzohaibmalik10@gmail.com>
2026-05-29 23:02:11 +0500

    added api-gateway url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/v1",
+  baseURL: "https://api-gateway-to6i.onrender.com/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
